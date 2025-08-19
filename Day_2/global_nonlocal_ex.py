979f1eb5e0763b1330f29dd20afbcbe42b041ef2
Vadim Shikhovtsov <5208643@mail.ru>
2025-08-19 12:24:05 +0300

    Add global, nonlocal and decorator examples

---
+++ Day_2/global_nonlocal_ex.py
@@ -0,0 +1,55 @@
+from pprint import pprint
+# Пример класа-декоратора
+# from functools import total_ordering
+
+s1 = 'hello'
+s2 = 'python'
+a = 4
+b = 8.4
+
+# 4 области видимости LEGB (Local, Enclosed, Global, Builtin)
+
+# pprint(globals()) # Словарь всех глобальных переменных
+
+def func_one():
+    a = 99
+    b = 22 # свободная переменная
+    # pprint(locals()) # Словарь всех локальных переменных  
+    # print(a)
+    # a += 1 # a = a + 1
+    def inner_func():
+        nonlocal a       
+        print(f'{locals() = }')
+        a = 2 + b
+        return a
+    return inner_func
+
+
+
+# res = func_one()
+
+# print(f'{a = }')
+# print(type(res))
+# print(res())
+
+# Пример декоратора
+def rub_to_usd(function_as_param):
+    def wrapper(*args, **kwargs):
+        # print(f'{locals() = }')
+        result = function_as_param(*args, **kwargs)
+        new_result = float(result[:-3]) / 93
+        return f"{new_result:.2f} EUR"
+    return wrapper
+
+
+@rub_to_usd  # func_sum = rub_to_usd(func_sum)
+def func_sum(price, count):
+    """ Return data format: <float> RUB"""
+    return f"{price * count * 1.18} RUB"
+
+print(func_sum(100, 10))
+print(func_sum)
+
+# Использование функции-декоратора без синтаксического сахара
+# func_sum_usd = rub_to_usd(func_sum)
+# print(func_sum_usd(100, 10))
