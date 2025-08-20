8f4cebd0e3adf7b8814fc12899bcc00e59ea529b
Vadim Shikhovtsov <5208643@mail.ru>
2025-08-20 12:35:57 +0300

    Add Task class tests

--- Day_3/pytest_materials/Lection2/test_task.py
+++ Day_3/pytest_materials/Lection2/test_task.py
@@ -15,3 +15,45 @@ def test_defaults():
     assert task.owner is None
     assert task.state == "todo"
     assert task.id is None
+
+
+def test_equility():
+    task1 = Task("important task", "petr", "todo", 123)
+    task2 = Task("important task", "petr", "todo", 123)
+    assert task1 == task2
+
+
+def test_equility_with_diff_ids():
+    task1 = Task("important task", "petr", "todo", 123)
+    task2 = Task("important task", "petr", "todo", 467)
+    assert task1 == task2
+
+
+def test_inequility():
+    task1 = Task("important task", "petr", "todo", 123)
+    task2 = Task("unimportant task", "petr", "todo", 123)
+    assert task1 != task2
+
+
+def test_task_from_dict():
+    task1 = Task("important task", "petr", "todo", 123)
+    task2_dict = {
+        "summary": "important task",
+        "owner": "petr", 
+        "state": "todo",
+        "id": 123,
+    }
+    task2 = Task.from_dict(task2_dict)
+    assert task1 == task2
+
+
+def test_task_to_dict():
+    task1 = Task("important task", "petr", "todo", 123)
+    task2_dict = task1.to_dict()
+    task2_expected = {
+        "summary": "important task",
+        "owner": "petr", 
+        "state": "todo",
+        "id": 123,
+    }
+    assert task2_dict == task2_expected
