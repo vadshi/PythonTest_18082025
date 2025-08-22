38cc2b8aa3faaba7c4f09841251bf32d3d0f8188
Vadim Shikhovtsov <5208643@mail.ru>
2025-08-22 10:56:59 +0300

    Finish API creating

--- Day_4/flask_user_management/app.py
+++ Day_4/flask_user_management/app.py
@@ -1,3 +1,4 @@
+from typing import Any
 from flask import Flask, jsonify, request
 from dotenv import load_dotenv
 from models import db, User
@@ -10,7 +11,8 @@ def create_app(app_config=None):
     app = Flask(__name__)
 
     # Get database URI from environment variables or use default
-    DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///:memory:")
+    # To create in memory: "sqlite:///:memory:"
+    DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///./test_db") # 
 
     if app_config:
         app.config.update(app_config)
@@ -18,14 +20,9 @@ def create_app(app_config=None):
         app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
 
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False    
-    app.config["SQLALCHEMY_ECHO"] = True
+    app.config["SQLALCHEMY_ECHO"] = False # To see raw SQL
 
     db.init_app(app)
-
-    @app.route("/", methods=["GET"])
-    def hello_world():
-        return jsonify("Hello, World!"), 200
-
 
     @app.get("/users")
     def get_users():
@@ -42,11 +39,11 @@ def create_app(app_config=None):
 
     @app.post("/users")
     def add_user():
-        data = request.json # the same as request.get_json()
-        new_user = User(username=data["username"], email=data["email"])  # User(**data)
+        data: dict[str, Any] = request.json # type: ignore # the same as request.get_json()
+        new_user = User(**data)
         db.session.add(new_user)
         db.session.commit()
-        return jsonify({"message": "User created with id={new_user.id}"}), 201
+        return jsonify({"message": f"User created with id={new_user.id}"}), 201
 
     return app
 
