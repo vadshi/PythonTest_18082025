1416578e534f47fe7839c1998ebf94162b8e852c
Vadim Shikhovtsov <5208643@mail.ru>
2025-08-22 11:30:59 +0300

    Add add_user test

---
+++ Day_4/flask_user_management/tests/conftest.py
@@ -0,0 +1,70 @@
+import pytest
+import random
+from sqlalchemy import StaticPool, create_engine
+from sqlalchemy.exc import OperationalError as SQLAlchemyOperationalError
+from flask_user_management.app import create_app
+from flask_user_management.models import db
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--dburl", # For Postgres use "postgres://user:password@host/dbname"
+        action="store",
+        default="sqlite:///:memory:", # Default use SQLite in-memory database,
+        help="Database URL use to tests",
+    )
+
+@pytest.fixture(scope="session")
+def db_url(request):
+    return request.config.getoption("--dburl")
+
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_sessionstart(session):
+    db_url = session.config.getoption("--dburl")
+    try:
+        # Attempt to create an engine and connect to the database
+        engine = create_engine(
+            db_url,
+            poolclass=StaticPool,
+        )
+        connection = engine.connect()
+        connection.close() # Clone the connection right after a successfull connect
+        print("Using Database URL", db_url)
+        print("Database connection successful ...")
+    except SQLAlchemyOperationalError as e:
+        print(f"Failed to connect to the database at {db_url}: {e}")
+        pytest.exit(reason="Stopping tests because database connection could not be established")
+
+
+@pytest.fixture(scope="session")
+def app(db_url):
+    """ Session-wide test 'app' fixture """
+    test_config = {
+        "SQLALCHEMY_DATABASE_URI": db_url,
+        "SQLALCHEMY_TRACK_MODIFICATIONS": False,
+    }
+    app = create_app(app_config=test_config)
+
+    with app.app_context():
+        db.create_all()
+        yield app
+
+        # Close the database session and drop all tables after the session
+        db.session.remove()
+        db.drop_all()
+
+
+@pytest.fixture
+def test_client(app):
+    """ Test client for the app """
+    return app.test_client()
+
+
+@pytest.fixture
+def user_payload():
+    suffix = random.randint(1, 100)
+    return {
+        "username": f"user_{suffix}",
+        "email": f"user_{suffix}@mail.com",
+    }
---
+++ Day_4/flask_user_management/tests/test_user_manager.py
@@ -0,0 +1,19 @@
+import json
+
+
+def test_add_user(test_client, user_payload):
+    response = test_client.get(
+        "/users", data=json.dumps(user_payload), content_type = "application/json"
+    )
+
+    assert response.status_code == 201
+    create_response_json = json.loads(response.data)
+    assert create_response_json.keys() == dict(message="User created").keys() 
+    assert "User created with id" in create_response_json.get("message")
+
+    response = test_client.get("/users")
+    assert response.status_code == 200
+
+    read_response_json = json.loads(response.data)
+    print(f'{read_response_json = }')
+    assert len(read_response_json) == 1
