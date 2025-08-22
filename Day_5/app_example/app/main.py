2d1dda336c337b72ef1657c35c16217a380a8704
Vadim Shikhovtsov <5208643@mail.ru>
2025-08-22 14:27:55 +0300

    Add and get items API

--- Day_5/app_example/app/main.py
+++ Day_5/app_example/app/main.py
@@ -2,7 +2,8 @@ from fastapi import (
     Depends,
     FastAPI,
     HTTPException,
-    Request
+    Request,
+    status
 )
 
 # library for validation, serialiazation and deserialization
@@ -17,3 +18,43 @@ app = FastAPI()
 @app.get("/home")
 def get_home():
     return {"message": "Hello, world!"}
+
+
+class ItemSchema(BaseModel):
+    name: str
+    color: str
+
+
+def get_db_session():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post("/item", response_model=int, status_code=status.HTTP_201_CREATED)
+def add_item(
+    item: ItemSchema,
+    db_session: Session = Depends(get_db_session)
+):
+    db_item = Item(name=item.name, color=item.color)
+    db_session.add(db_item)
+    db_session.commit()
+    db_session.refresh(db_item)
+    return db_item.id
+
+
+@app.get("/item/{item_id}", response_model=ItemSchema)
+def get_item(item_id: int, db_session: Session = Depends(get_db_session)):
+    item_db = (
+        db_session.query(Item)
+        .filter(Item.id == item_id)
+        .first()
+    )
+    if item_db is None:
+        raise HTTPException(status_code=404, detail=f"Item with id={item_id} not found")
+
+    return item_db
+
+
